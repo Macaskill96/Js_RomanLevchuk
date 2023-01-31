293e1a1ec599df8c686821108ecc840053b14fea
Macaskill96 <levchuk.r.s.96@gmail.com>
2023-02-01 00:40:53 +0200

    Виконав ДЗ урок 1

---
+++ Lessons/Lessons1/main.js
@@ -0,0 +1,43 @@
+let world = 'hello'
+console.log(world);
+
+let site = 'owu'
+console.log(site);
+
+let dot = 'com'
+console.log(dot);
+
+let ukr = 'ua'
+console.log(ukr);
+
+let num1 = 1
+console.log(num1);
+let num2 = 10
+console.log(num2);
+let num3 = -999
+console.log(num3);
+let num4 = 123
+console.log(num4);
+let num5 = 3.14
+console.log(num5);
+let num6 = 2.7
+console.log(num6);
+let num7 = 16
+console.log(num7);
+let pravda = true
+console.log(pravda);
+let brehnya = false
+console.log(brehnya);
+
+let firstName = 'Roman';
+let middleName = 'Sergiyovich';
+let lastName = 'Levchuk';
+
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+let a = 100;
+console.log(typeof a);
+let b = '100'
+console.log(typeof b);
+let c = true
+console.log(typeof c);
