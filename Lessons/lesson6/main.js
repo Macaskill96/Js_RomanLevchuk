222ebbc0047c60f7d375ba56bc17f90fae347c41
Macaskill96 <levchuk.r.s.96@gmail.com>
2023-02-20 13:20:52 +0200

    вмконав дз 6 уроку. + 1завдання

--- Lessons/lesson6/main.js
+++ Lessons/lesson6/main.js
@@ -325,8 +325,8 @@ let cards = [{
 // let diamondAll = cards.filter(c => c.cardSuit === 'diamond')
 // console.log(diamondAll);
 
-// let trefsNinePlus = cards.filter(c => c.value >= '9' && c.cardSuit ==='clubs')
-// console.log(trefsNinePlus);
+// let trefsNinePlus = cards.filter(c => (c.value > '8' || c.value === '10') && c.cardSuit === 'clubs'  )
+// console.log(trefsNinePlus)
 
 
 //     Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
