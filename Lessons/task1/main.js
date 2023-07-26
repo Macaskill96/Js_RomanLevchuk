dd32daf44b49ce3fed33f13244564a9dfb37685b
Macaskill96 <levchuk.r.s.96@gmail.com>
2023-07-27 01:40:34 +0200

    task1 - commit2

--- Lessons/task1/main.js
+++ Lessons/task1/main.js
@@ -11,7 +11,6 @@ let notes = [
     {date: '28.06.2017', note: 'Cooking', category:'Idea'}
 ];
 
-
 function formatDate () {
     const YYYY = new Date().getFullYear().toString();
     const MM = (new Date().getMonth() + 1).toString().padStart(2, '0');
@@ -20,10 +19,9 @@ function formatDate () {
     return {YYYY, MM, DD};
 }
 function findDates(text) {
-    const dateRegex = /\b\d{2}[-.]\d{2}[-.]\d{4}\b/g;
+    const dateRegex = /\b\d{2}[-./]\d{2}[-./]\d{4}\b/g;
     return text.match(dateRegex) || [];
 }
-
 
 let list = document.getElementById('categorySelect');
 let select = category.map(c => {
@@ -32,10 +30,12 @@ let select = category.map(c => {
     list.appendChild(optionElement)
 });
 
+
+
 function CreateBlockNotes () {
     const notesContainer = document.getElementById('notesContainer');
     notesContainer.innerHTML = '';
-    let divNotes = notes.map(note => {
+    let divNotes = notes.map((note, index) => {
         let container = document.createElement('div');
         container.classList.add('noteDiv');
         let divDate = document.createElement('div');
@@ -49,49 +49,116 @@ function CreateBlockNotes () {
         let forButton = document.createElement('div')
         forButton.classList.add('forButton')
 
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            Delete(index);
+        });
+
+        let updateButton = document.createElement('button');
+        updateButton.classList.add('button');
+        updateButton.innerText = 'Update'
+        updateButton.addEventListener('click', () => {
+         Update(index);
+        })
+
         let filter = note.note
         let foundDate = findDates(filter);
-        console.log(foundDate);
 
         divDate.innerText = note.date;
         divNotes.innerText = note.note;
         divCategory.innerText = note.category;
-        forButton.innerHTML = '<button class="button">Delete</button> <button class="button">Edit</button>'
+        divDates.innerText = foundDate
+        forButton.append(deleteButton, updateButton)
         container.append(divDate, divNotes, divCategory, divDates, forButton);
-        notesContainer.appendChild(container)
+        notesContainer.appendChild(container);
     })
 }
-CreateBlockNotes();
 
-let newArr = [];
-const button = document.getElementById('button')
-button.addEventListener('click', SaveData)
+
 let inputData = document.getElementById('inputSelect');
+const button = document.getElementById('button')
+let changeText = true;
+
+function ChangeButton () {
+    const action = button.getAttribute('data-action')
+    if (action === 'create') {
+        button.innerText = 'Create';
+        button.disabled = changeText;
+        button.removeEventListener('click', SaveNewNote);
+        button.addEventListener('click', SaveData)
+    } else if (action === 'update') {
+        button.innerText = 'Save'
+        button.disabled = changeText;
+        button.removeEventListener('click', SaveData)
+        button.addEventListener('click', SaveNewNote)
+    }
+}
+ChangeButton();
 
 function SaveData(e) {
     e.preventDefault();
+    button.setAttribute('data-action', 'create');
     let formData = formatDate();
     let category = document.getElementById('categorySelect').value;
     let note = document.getElementById('inputSelect').value;
-    let date = `${formData.DD}.${formData.MM}.${formData.YYYY}`
+    let date = `${formData.DD}.${formData.MM}.${formData.YYYY}`;
+    let newData = {date, note, category};
 
-    const newData = {date, note, category};
-    newArr.push(newData);
-    notes.push(...newArr);
-    newArr = [];
-
+    notes.push(newData);
     document.getElementById('categorySelect').value = 'Task';
     document.getElementById('inputSelect').value = '';
-    button.disabled = true;
+    button.disabled = false;
     CreateBlockNotes();
+
+    changeText = false;
+    ChangeButton();
 
 };
 
 
+function Delete (index) {
+    notes.splice(index,1)
+    CreateBlockNotes();
+}
 
 
+let valueIndex;
+function Update (index) {
+    button.setAttribute('data-action', 'update')
+    const test = notes.map((note, index) => {
+        return `${note.note}`
+    })
+
+    let oldInput = inputData.value = test[index];
+    let indexOf = test.indexOf(test[index])
+    valueIndex = indexOf;
+    button.disabled = true;
+    changeText = false;
+    ChangeButton();
+}
 
 
+function SaveNewNote (e) {
+    e.preventDefault();
+    let index = valueIndex;
+    const valueInput = document.getElementById('inputSelect').value;
+
+    if (index >=0 && index < notes.length) {
+        notes[index].note = valueInput;
+        document.getElementById('categorySelect').value = 'Task';
+        document.getElementById('inputSelect').value = '';
+        button.disabled = true;
+        CreateBlockNotes();
+    } else {
+        console.error('You have a problem');
+    }
+
+    button.setAttribute('data-action', 'create');
+    changeText = true;
+    ChangeButton();
+}
 
 inputData.addEventListener('input', function () {
     if (inputData.value.trim() !== '') {
@@ -100,42 +167,5 @@ inputData.addEventListener('input', function () {
         button.disabled = true;
     }
 })
+CreateBlockNotes();
 
-
-//
-// let container = document.createElement('div');
-// container.classList.add('container')
-// document.body.appendChild(container);
-//
-// let form = document.createElement('div');
-// container.appendChild(form);
-//
-// let inputs = document.forms.formForNote;
-// inputs.onsubmit = function (eo) {
-//     eo.preventDefault();
-//
-//     let forDate = document.createElement('div');
-//     let dateNote = document.createElement('div');
-//     let forNote = document.createElement('div');
-//     let forCategory = document.createElement('div');
-//
-//     let selectedCategory = list.options[list.selectedIndex].text;
-//     let noteText = document.createElement('p');
-//     noteText.innerText = `${inputs.note.value}`;
-//
-//     let date = formatDate();
-//     dateNote.innerText = `${date.YYYY}, ${date.MM}, ${date.DD}`;
-//     forDate.append(dateNote);
-//
-//     forNote.appendChild(noteText);
-//     forCategory.innerText = selectedCategory;
-//
-//     let entryBlock = document.createElement('div');
-//     entryBlock.classList.add('entry-block');
-//     entryBlock.append(forDate, forNote, forCategory);
-//
-//     container.appendChild(entryBlock);
-//
-//
-//     inputs.note.value = '';
-// };
